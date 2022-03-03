48c351b419c3d84323f90ad7931a19b05ac79c46
Datta Sanjay Jaware <72984745+DattaJaware@users.noreply.github.com>
2022-03-03 19:30:26 +0530

    MVC

---
+++ TestApp/MVCTestApp/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using MVCTestApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTestApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        TestAppDbEntities db = new TestAppDbEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult CheckLogin() {
+            return Json("",JsonRequestBehavior.AllowGet);
+        }
+    }
+}
